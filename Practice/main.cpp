9b42138136004947aee07ca47820a04e3299a815
ReyRen <reyren@reypro.fios-router.home>
2017-09-26 01:34:24 -0400

    Initial Commit

---
+++ Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by Rey Ren on 2017/9/26.
+//  Copyright © 2017年 Rey Ren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
